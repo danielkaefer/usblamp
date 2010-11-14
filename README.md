fc3a36eb44a66e3af2d1cd74906cbbe6f044bd69
Daniel Kaefer <spam10@0x15.de>
2010-11-14 01:28:30 +0100

    prepare for release

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 WebMail Notifier Linux driver
 =============================
 
+Usage
+-----
+
+sudo ./usblamp red
+sudo ./usblamp off
+
 License
 -------
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,62 @@
-/* 
- * File:   main.c
- * Author: daniel
+/*
+ * Copyright (C) 2010  Daniel Kaefer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Created on 20. Juni 2010, 19:57
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <usb.h>
+#include <string.h>
 
 #include "USBLamp.hpp"
 
 //using namespace std;
 
-int main(int argc, char** argv) {
-    printf("start\n");
+void setColor(Color c) {
     USBLamp lamp = USBLamp();
     lamp.open();
     if (lamp.isConnected()) {
-        while(1) {
-            Color c = Color(0,0,127);
-            lamp.setColor(c);
-            sleep(1);
-            c = Color(127,0,0);
-            lamp.setColor(c);
-            sleep(1);
-            c = Color(0,60,60);
-            lamp.setColor(c);
-            sleep(1);
-        }
-        lamp.switchOff();
+        //c = Color(0,60,60);
+        lamp.setColor(c);
         //lamp.sendInterrupt();
         lamp.close();
     } else {
         printf("no lamp found\n");
     }
+
+}
+
+int main(int argc, char** argv) {
+    printf("start\n");
+
+    if(argc == 2) {
+        if(strcmp(argv[1], "red") == 0) {
+            setColor(Color(127,0,0));
+        } else if(strcmp(argv[1], "green") == 0) {
+            setColor(Color(0,127,0));
+        } else if(strcmp(argv[1], "blue") == 0) {
+            setColor(Color(0,0,127));
+        } else {
+            // default set off
+            setColor(Color(0,0,0));
+        }
+    } else {
+        printf("usage\n");
+    }
+
     printf("end\n");
 
     return 0;
